2138e9117eb5b2331ab2ba1225e6e5a55c0973bc
user-jkj <s2331099uy@s.chibakoudai.jp>
2024-10-26 20:02:03 +0900

    Update testscript001.sh

--- testscript001.sh
+++ testscript001.sh
@@ -9,15 +9,15 @@ rm -f ${FILE}
 for request in 10 20 30 40 50 100 200 300 400 500 600 700 900 1000
 do
         sum=0
-        ave=0
         for i in `seq 5`
         do
                 resolt=$(ab -c 10 -n ${request} ${URL} | grep 'Requests per second' | sed -r 's/.*( +)([0-9]+\.[0-9][0-9]) .*/\2/')
                 
-                sum=$(echo "$sum + $resolt "|bc)
+                sum=$(echo "$sum + $resolt"|bc)
         done
 
         ave=$(echo "$sum / 5" | bc)
-        echo -n ${request}' ' >> ${FILE}
-        echo $ave >> ${FILE}     
+        echo ${request} >> ${FILE}
+        echo ${ave} >> ${FILE}     
 done
+
