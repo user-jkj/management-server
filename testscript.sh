5f2f59267f674a37d133c423d4e4a35b8a73639e
user-jkj <s2331099uy@s.chibakoudai.jp>
2024-10-27 00:01:31 +0900

    Add files via upload

---
+++ testscript.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#testscript001.sh
+
+URL=http://127.0.0.1/data
+FILE=testdata1.dat
+
+rm -f ${FILE}
+
+for request in 10 20 30 40 50 100 200 300 400 500 600 700 900 1000
+do
+        sum=0
+        for i in `seq 5`
+        do
+                sum=$(echo "$sum + $(ab -c 10 -n ${request} ${URL} | grep 'Requests per second' | sed -r 's/.*( +)([0-9]+\.[0-9][0-9]) .*/\2/')"|bc)
+        done
+
+        ave=$(echo "$sum / 5" | bc)
+        echo ${request} >> ${FILE}
+        ${ave} >> ${FILE}     
+done
+
