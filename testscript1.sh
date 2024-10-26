bc78fe410c8b366f4aacca1ce063360ef429af21
user-jkj <s2331099uy@s.chibakoudai.jp>
2024-10-26 16:40:08 +0900

    Update testscript1.sh

--- testscript1.sh
+++ testscript1.sh
@@ -7,7 +7,7 @@ FILE=test1.dat
 rm -f ${FILE}
 
 
-for request in 10 20 30 40 50 100 200 300 400 500 600 700 900 1000
+for request in 50 100 200 300 400 500 600 700 900 1000
 do
         echo -n ${request}' ' >> ${FILE}
         ab -c 10 -n ${request} ${URL} | grep 'Requests per second' | sed -r 's/.*( +)([0-9]+\.[0-9][0-9]) .*/\2/' >> ${FILE}
